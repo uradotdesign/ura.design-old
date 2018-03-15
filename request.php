fb830adc2c441c3c5016ead0634593db7588da7d
Anxhelo Lushka <anxhelo1995@gmail.com>
2018-03-15 17:52:28 +0100

    Update request.php

--- request.php
+++ request.php
@@ -2,54 +2,6 @@
 layout: default
 section_id: contact
 ---
-
-<?php
-/**
- * This example shows how to handle a simple contact form.
- */
-$msg = '';
-//Don't run this unless we're handling a form submission
-    require '../PHPMailerAutoload.php';
-    //Create a new PHPMailer instance
-    $mail = new PHPMailer;
-    //Tell PHPMailer to use SMTP - requires a local mail server
-    //Faster and safer than using mail()
-    $mail->isSMTP();
-    $mail->Host = 'localhost';
-    $mail->Port = 25;
-    //Use a fixed address in your own domain as the from address
-    //**DO NOT** use the submitter's address here as it will be forgery
-    //and will cause your messages to fail SPF checks
-    $mail->setFrom('team@ura.design', 'Ura Design');
-    //Send the message to yourself, or whoever should receive contact for submissions
-    $mail->addAddress('anxhelo1995@gmail.com', 'Anxhelo Lushka');
-    //Put the submitter's address in a reply-to header
-    //This will fail if the address provided is invalid,
-    //in which case we should ignore the whole request
-    if ($mail->addReplyTo($_POST['email'], $_POST['name'])) {
-        $mail->Subject = 'PHPMailer contact form';
-        //Keep it simple - don't use HTML
-        $mail->isHTML(false);
-        //Build a simple message body
-        $mail->Body = <<<EOT
-Email: {$_POST['email']}
-Name: {$_POST['name']}
-Message: {$_POST['message']}
-EOT;
-        //Send the message, check for errors
-        if (!$mail->send()) {
-            //The reason for failing to send will be in $mail->ErrorInfo
-            //but you shouldn't display errors to users - process the error, log it on your server.
-            $msg = 'Sorry, something went wrong. Please try again later.';
-        } else {
-            $msg = 'Message sent! Thanks for contacting us.';
-        }
-    } else {
-        $msg = 'Invalid email address, message ignored.';
-    }
-}
-?>
-
 <div class="three spacing"></div>
 <div class="full">
       <div class="large-9 large-centered columns">
@@ -61,7 +13,7 @@ EOT;
   <div class="row">
     <div class="large-10 large-centered columns">
       <div class='form'>
-        <form id='contact_form' method='POST'>
+        <form id='contact_form' method='POST' action="form.php">
           <p id='thanks' class='hide'>
             Thanks for contacting us, we'll be in touch soon!
           </p>
@@ -96,4 +48,4 @@ EOT;
     </div>
   </div>
 </div>
-      <div class="three spacing"></div>
+<div class="three spacing"></div>
