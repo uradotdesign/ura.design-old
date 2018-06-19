97815fb224d5a97d6e61a1276b992aa3f3b9896e
Anxhelo Lushka <anxhelo1995@gmail.com>
2018-06-19 21:12:06 +0200

    test for new cookie bar

--- js/svg.js
+++ js/svg.js
@@ -34,3 +34,12 @@ jQuery('img.svg').each(function(){
     }, 'xml');
 
 });
+
+$(document).ready(function(){
+    setTimeout(function () {
+        $("#cookieConsent").fadeIn(200);
+     }, 4000);
+    $("#closeCookieConsent, .cookieConsentOK").click(function() {
+        $("#cookieConsent").fadeOut(200);
+    });
+}); 
